44d4bae48e85f96a79dd7b37b2596c8f2bafb199
rubyTanuki <161648546+rubyTanuki@users.noreply.github.com>
2025-11-08 16:41:13 -0600

    apiController init

--- HackUTD2025API/Program.cs
+++ HackUTD2025API/Program.cs
@@ -2,19 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.MapControllers();
-
-app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
